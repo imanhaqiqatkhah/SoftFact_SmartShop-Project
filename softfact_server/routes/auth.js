da62db5f59dc375e53a2c712526fa10a87895406
imanhaqiqatkhah <imanhaqiqatkhah@gmail.com>
2025-02-04 20:09:16 +0330

    added user models

--- softfact_server/routes/auth.js
+++ softfact_server/routes/auth.js
@@ -6,7 +6,10 @@ router.post("/login", (req, res) => {
   return res.status(201).json({ name: "iman", age: 32 })
 })
 
-router.post("/register", (req, res) => {})
+router.post("/register", (req, res) => {
+  // validate the user
+  // tap into the database and create a new USER in the users collection in the database
+})
 
 router.post("/forgot-password", (req, res) => {})
 
